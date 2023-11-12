c99cad6908bbe23c52929fb726b178f61489214d
Mikhail Biriukov <12212005b@gmail.com>
2023-11-12 23:14:49 +0300

    корень, степень. тетрация

--- Model/src/OperationKeeper.cpp
+++ Model/src/OperationKeeper.cpp
@@ -5,6 +5,9 @@ namespace Model {
 		operations.push_back(new Subtraction);	
 		operations.push_back(new Multiply);
 		operations.push_back(new Division);
+		operations.push_back(new Root);
+		operations.push_back(new Poww);
+		operations.push_back(new Tetration);
 	}
 
 	const std::list<Operation *> &OperationKeeper::getOperations() const {
--- Model/src/RealOperations.cpp
+++ Model/src/RealOperations.cpp
@@ -49,4 +49,42 @@ namespace Model {
 	double Division::execute(double a, double b) const {
 		return a / b;
 	}
+
+	Root::Root() :Operation("sqrt") {}
+
+	double Root::execute(double a, double b) const
+	{
+		if (b == 0) {
+			return 1;
+		}
+		if (a == 0) {
+			return a;
+		}
+		if (b == 1) {
+			return a;
+		}
+		else {
+			return pow(a, 1 / b);
+		}
+	}
+
+	Poww::Poww() :Operation("**") {}
+
+	double Poww::execute(double a, double b) const
+	{
+		return pow(a, b);
+	}
+
+	Tetration::Tetration() : Operation("^_^"){}
+
+	double Tetration::execute(double a, double b) const {
+		double tmp = a;
+		for (int i = 0; i < b; i++) {
+			tmp = pow(tmp, a);
+		}
+		return tmp;
+		
+	}
+
+	
 }
